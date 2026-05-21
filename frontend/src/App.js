bb1bc9398d196615db148515b261f38171bce7c5
a.cheskaa_ <cheska_mascarinas@yahoo.com>
2026-05-21 18:46:53 +1000

    adding restrictions for each page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -38,7 +38,6 @@ export default function App() {
             const roleNum = Number(userRole);
             setRole(roleNum);
 
-            if (roleNum === 0) setPage("none");
             if (roleNum === 1) setPage("miner");
             if (roleNum === 2) setPage("certifier");
             if (roleNum === 3) setPage("grader");
@@ -55,17 +54,38 @@ export default function App() {
         }
     },[]);
 
+    function unauthorised() {
+        return (
+            <div className="page">
+                <h1> Access Denied</h1>
+                <p>You are not authorised to access this page.</p>
+            </div>
+        );
+    }
 
     function renderPage() {
-        switch (page) {
-            case "startup": return<StartupPage onGoConfig={() => setPage("config")} onGoMint={() => setPage("mint")} />;
-            case "config": return<StakeholderConfigPage onBack={() => setPage("startup")} />;
-            case "mint": return<MintPage onNext={() => setPage("diamond")} onBack={() => setPage("startup")} />;
-            case "diamond": return<DiamondMainPage onNext={() => setPage("polishing")} onBack={() => setPage("mint")} />;
-            case "polishing": return<PolishingPage onNext={() => setPage("market")} onBack={() => setPage("diamond")} />;
-            case "market": return<MarketPage onBack={() => setPage("startup")} />;
-            default: return<StartupPage />;
+
+        if (role === 1) {
+        // miner can only access mint and diamond pages
+            switch (page) {
+                case "mint": return <MintPage onNext={() => setPage ("diamond")} onBack{() => setPage("mint")} />;
+                case "diamond": return <DiamondMainPage onNext={() => setPage("diamond")} onBack={() => setPage("mint")} />;
+                default: return <MintPage> onNext{() => setPage("diamond")} onBack={() => setPage("mint")} />;
+            }
+
+        if (role === 2) {
+            // Kimberley Certifier can only access diamond page
+            return <DiamondMainPage onNext{() => setPage("diamond")} onBack={() => setPage("diamond")} />;
         }
+
+        if (role === 3)
+            // Grader/Polisher can only access polishing and market page
+            case "polishing": return <PolishingPage onNext={() => setPage("market")} onBack={() => setPage("polishing")} />;
+            case "market": return<MarketPage onBack={() => setPage("polishing")} />;
+            default: return <PolishingPage onNext={() => setPage("market")} onBack={() => setPage("polishing")} />;
+        }
+
+        return unauthorised();
     }
     return (
         <div className = "app">
