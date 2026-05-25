e795df206fdd2197429e82dd69cd1065aef409fb
a.cheskaa_ <cheska_mascarinas@yahoo.com>
2026-05-25 18:33:46 +1000

    adding aesthetics for StartupPage.css

--- frontend/src/pages/StartupPage.jsx
+++ frontend/src/pages/StartupPage.jsx
@@ -1,13 +1,31 @@
+import "./StartupPage.css";
+import minerImg from "../img/miner-button.png";
+import certifyImg from "../img/certify-button.png";
+import gradeImg from "../img/grade.polish-button.png";
+
 export default function StartupPage({ role, onSelectRole}) {
+    const roleLabel =
+        role === 1 ? "Miner" :
+        role === 2 ? "Kimberley Certifier" :
+        role === 3 ? "Grader" : "connecting...";
+
     return (
-        <div className="page">
-            <h1>gemvault</h1>
-            <p>Diamond Supply Chain</p>
+        <div className ="startup-wrapper">
 
-            <button onClick={() => onSelectRole("miner")}>Mining</button>
-            <button onClick={() => onSelectRole("certifier")}>Kimberley Certification</button>
-            <button onClick={() => onSelectRole("grader")}>Grade/Polish</button>
+                    {/* Full background image */}
+                    <div className="startup-hero">
 
+                    {/* Welcome text */}
+                    <h1 className="welcome-text">Welcome back,</h1>
+                    <h2 className ="role-text">{roleLabel} .</h2>
+
+                    {/* Clickable image cards */}
+                    <div className="startup-cards">
+                        <img src={minerImg} alt="Mine a Diamond" className="role-card" onClick={() => onSelectRole("miner")} />
+                        <img src={certifyImg} alt="Certify Diamond" className="role-card" onClick={() => onSelectRole("certifier")} />
+                        <img src={gradeImg} alt="Grade/Polish" className="role-card" onClick={() => onSelectRole("grader")} />
+                    </div>
+                </div>
         </div>
     );
-    }
+}
